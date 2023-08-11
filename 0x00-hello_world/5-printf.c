b613e2d66a7268dff3c4bda1f463f93f39b07511
lamiaibrahim-aech <Lamiaibrahim48@gmail.com>
2023-08-10 17:41:11 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#incloud <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: 0 when successfull
+ */
+
+int main(void)
+{
+	peintf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
