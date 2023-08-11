7f1f0d86fd8f798b02c8578b870a7a1866831d9f
lamiaibrahim-aech <Lamiaibrahim48@gmail.com>
2023-08-10 17:34:27 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+
 /**
- * main - this is main function
+ * main - this is the main function
  *
  * Return: always 0
  */
+
 int main(void)
 {
-      puts("\"Programming is like building a multilingual puzzle");
-	      retuen (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
